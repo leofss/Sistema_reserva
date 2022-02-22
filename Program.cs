e78bbe882717d4b32af357333cb26f5b97316648
leonardo fernandes <leofernandess11@gmail.com>
2022-02-22 15:55:43 -0300

    Implementação do tratamento de exceções

--- Program.cs
+++ Program.cs
@@ -1,19 +1,41 @@
 ﻿using System;
-
+using Sistema_reserva.Entities.Exceptions;
 namespace Sistema_reserva
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Numero da sala");
-            int num = int.Parse(Console.ReadLine());
-            Console.Write("Checkin data");
-            DateTime checkin = DateTime.Parse(Console.ReadLine());
-            Console.Write("Checkou data");
-            DateTime checkout = DateTime.Parse(Console.ReadLine());
+            try
+            {
+                Console.WriteLine("Numero da sala");
+                int num = int.Parse(Console.ReadLine());
+                Console.Write("Checkin data");
+                DateTime checkin = DateTime.Parse(Console.ReadLine());
+                Console.Write("Checkou data");
+                DateTime checkout = DateTime.Parse(Console.ReadLine());
 
-            Reservation reservation = new Reservation(num, checkin, checkout);
+                Reservation reservation = new Reservation(num, checkin, checkout);
+                Console.WriteLine($"Reserva: {reservation}");
+
+                Console.WriteLine("");
+                Console.WriteLine("Entre data para atualizar a reserva: ");
+                Console.Write("Check in data (dd/MM/yyyy): ");
+                checkin = DateTime.Parse(Console.ReadLine());
+                Console.WriteLine("Check out data (dd/MM/yyyy): ");
+                checkout = DateTime.Parse(Console.ReadLine());
+
+                reservation.UpdateDates(checkin, checkout);
+                Console.WriteLine($"Reserva: {reservation}");
+            }
+            catch(DomainEx e)
+            {
+                Console.WriteLine($"Erro na reserva {e.Message}");
+            }
+            catch(FormatException e)
+            {
+                Console.WriteLine($"Erro na entrada do numero da sala: {e.Message}");
+            }
         }
     }
 }
