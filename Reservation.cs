21633dea379abd5c6d71d15fc7da54c94cec7cc7
leonardo fernandes <leofernandess11@gmail.com>
2022-02-22 15:03:20 -0300

    Adicionar arquivos de projeto.

---
+++ Reservation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sistema_reserva
+{
+    class Reservation
+    {
+        public int RoomNumber { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+           
+        public Reservation()
+        {
+
+        }
+
+        public Reservation(int roomNumber, DateTime checkIn, DateTime checkOut)
+        {
+            RoomNumber = roomNumber;
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public int Duration()
+        {
+            TimeSpan duration = CheckOut.Subtract(CheckIn);
+            return (int)duration.TotalDays;
+        }
+
+        public override string ToString()
+        {
+            return $"Quarto {RoomNumber}, checkin {CheckIn.ToString("dd/MM/yyyy")}, chekout {CheckOut.ToString("dd/MM/yyyy")}, duração {Duration()} noites ";
+        }
+    }
+}
